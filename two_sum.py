f9145596e6496d251a5fc978ff369e3517863f47
hugoRomero <romeroh862@gmail.com>
2019-08-08 20:06:20 -0700

    created the structure and simple solution to the problem. Function to create and process an array were created also the ability to allow the user to both choose the elements of the list and the sum they want to process. What is need is: error handling and ability to process the list more than once

---
+++ two_sum.py
@@ -0,0 +1,31 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up
+to a specific target.
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+"""
+def create_the_array():
+    my_array = []
+    input_from_user = None
+    while(input_from_user != 'x'):
+        print('please input a number for each array index. Press "x" when done')
+        input_from_user = input()
+        if(input_from_user!='x'):
+            my_array.append(int(input_from_user))
+
+    return my_array
+def add_two(an_array, number):
+    i = 0
+    x = 0
+    for x in range(len(an_array)):
+        if ((an_array[x] + an_array[x]+1) == number):
+            i = an_array[x]
+            x = an_array[x]+1
+
+    return i,x
+
+the_create_array = create_the_array()
+print(the_create_array)
+print('Please input a number that two indices of the array need to add up to:')
+sum = input()
+print(add_two(the_create_array,sum))
