f305ffd8923d8894f38b4a45a84cb081fb8cf286
hugoRomero <romeroh862@gmail.com>
2019-08-08 20:38:24 -0700

    second for loop added to traverse two indices at a time. still need to trouble shoot

--- two_sum.py
+++ two_sum.py
@@ -17,15 +17,21 @@ def create_the_array():
 def add_two(an_array, number):
     i = 0
     x = 0
-    for x in range(len(an_array)):
-        if ((an_array[x] + an_array[x]+1) == number):
-            i = an_array[x]
-            x = an_array[x]+1
+    for j in range(len(an_array)):
+        if ((an_array[j] + an_array[j]+1) == number):
+            i = j
+            x = j+1
+        else:
+            for k in range(len(an_array)):
+                if((an_array[j] + an_array[k]) == number):
+                    i = j
+                    x = k
 
     return i,x
 
 the_create_array = create_the_array()
 print(the_create_array)
 print('Please input a number that two indices of the array need to add up to:')
-sum = input()
+sum =int(input())
 print(add_two(the_create_array,sum))
+print(the_create_array[2])
