d9907c7ec29dd1e78737a2ac59afafc21cc225d2
hugoRomero <romeroh862@gmail.com>
2019-08-15 19:32:57 -0700

    creating a bst template to test the accuracy of the solution

--- find_the_value_of_nodes_in_bst.py
+++ find_the_value_of_nodes_in_bst.py
@@ -3,6 +3,32 @@ In this exercise, a function is created to traverse through a tree
 and return the sum of all the nodes
 """
 
+class node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.value = key
+
+def insert(root, node):
+    if root is None:
+        root = node
+    else:
+        if root.val < node.value:
+            if root.right is None:
+                root.right = node
+            else:
+                insert(root.right, node)
+        else:
+            if root.left is None:
+                root.left = node
+            else:
+                insert(root.left, node)
+
+
+
+
+
+
 class BST_soulution(object):
     def sum_BST(self,root,Left, Right):
         def dfs(node):
