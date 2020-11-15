3db5c932cbbbfa715fa207147ea38519b98b8cf6
r0meroh <romeroh862@gmail.com>
2020-11-14 16:24:03 -0800

    began a new folder with the excersises from a book of the same tittle

---
+++ daily_python_coding_problem/product_sum.py
@@ -0,0 +1,14 @@
+# this program takes in an array and returns a new array with the elements being the prodruct all of other elements but themselves
+
+def productSum (array):
+    prefix = []
+    suffix = []
+
+    for num in array:
+        
+        prefix.append(num)
+
+    return prefix
+
+
+
