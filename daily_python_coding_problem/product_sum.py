13ef84e899968b9dadfb625e479994078e620ea5
r0meroh <romeroh862@gmail.com>
2020-11-16 00:08:28 -0800

    added ideal solution

--- daily_python_coding_problem/product_sum.py
+++ daily_python_coding_problem/product_sum.py
@@ -12,7 +12,7 @@ def productSum (array):
             new_num += (num * nums )
         suffix.append(new_num)
 
-    
+
     print(suffix)
 
     for num in range(len(suffix)):
@@ -21,4 +21,36 @@ def productSum (array):
     print(suffix)
 
 
+
+def idealSolution(array):
+    prefix_products = []
+    for num in array:
+        if prefix_products:
+            prefix_products.append(prefix_products[-1] * num)
+        else:
+            prefix_products.append(num)
+
+    suffix_products = []
+
+    for num in reversed(array):
+        if suffix_products:
+            suffix_products.append(suffix_products[-1] * num)
+        else:
+            suffix_products.append(num)
+
+    result = []
+    for i in range(len(array)):
+        if i == 0:
+            result.append(suffix_products[i + 1])
+        elif i == len(array) - 1:
+            result.append(prefix_products[i - 1])
+        else:
+            result.append(prefix_products[i - 1] * suffix_products[i + 1])
+
+    print(result)
+
+
+
+
 productSum([2,4,6])
+idealSolution([2,4,6])
