ecbe6e4637e2cf6b36124e6637163731afe1b7fe
r0meroh <romeroh862@gmail.com>
2020-11-15 23:50:06 -0800

    added brute force to first problem

--- daily_python_coding_problem/product_sum.py
+++ daily_python_coding_problem/product_sum.py
@@ -1,14 +1,27 @@
 # this program takes in an array and returns a new array with the elements being the prodruct all of other elements but themselves
-
+# brute force O(n^2)
 def productSum (array):
     prefix = []
     suffix = []
 
+
     for num in array:
-        
+
         prefix.append(num)
 
-    return prefix
+    for num in prefix:
+        new_num = 0
+        for nums in array:
+            new_num += (num * nums )
+        suffix.append(new_num)
+
+    print(prefix)
+    print(suffix)
+
+    for num in range(len(suffix)):
+        suffix[num] = suffix[num] / array[num]
+
+    print(suffix)
 
 
-
+productSum([2,4,6])
