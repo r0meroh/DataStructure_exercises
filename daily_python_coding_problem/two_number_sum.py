d5b4c5ef6af53f676d556ff67bad7cd32619e668
r0meroh <romeroh862@gmail.com>
2020-11-16 13:01:34 -0800

    added set solution

--- daily_python_coding_problem/two_number_sum.py
+++ daily_python_coding_problem/two_number_sum.py
@@ -14,7 +14,27 @@ def two_number_sum(array, target):
     return pairs
 
 
+# solution with set O(n)
+def two_sum_with_set(array, target):
+    my_set = {}
+    my_set = set(array)
+    pairs = []
+    sub = 0
+    for num in array:
+        sub = target - num
+         # print(sub)
+        if sub in my_set and sub not in pairs:
+             # print(str(sub) + 'is in the set' + str(my_set))
+            pairs.append(num)
+            pairs.append(sub)
+            my_set.remove(sub)
+
+    return pairs
+
 
 if __name__ == '__main__':
     test = [1,2,3,4]
     print(two_number_sum(test, 5))
+    print(two_sum_with_set(test, 5))
+    print(two_number_sum([], 2))
+    print(two_sum_with_set([], 3))
