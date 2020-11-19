0594e443892eae00bd363ce951444f2e703aa42a
r0meroh <romeroh862@gmail.com>
2020-11-18 19:15:59 -0800

    added bst creation

---
+++ daily_python_coding_problem/bst_creation.py
@@ -0,0 +1,56 @@
+class bstNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            print('current node is root')
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = bstNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = bstNode(data)
+
+
+    # LnR
+    def in_order(self):
+        elements = []
+
+        if self.left:
+            print(self.data)
+            elements += self.left.in_order()
+
+        elements.append(self.data)
+
+        if self.right:
+            print(self.data)
+            elements += self.right.in_order()
+
+
+
+        return elements
+
+
+def build_bst(nodes):
+    root = bstNode(nodes[0])
+
+    for x in range(len(nodes)):
+        root.add_child(nodes[x])
+
+    return root
+
+
+if __name__ == '__main__':
+    nums = [1,2,11,33,333,4,41,9]
+
+    my_tree = build_bst(nums)
+    print(my_tree.in_order())
